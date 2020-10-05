c185e3db6e03b401e2d88fe29691cd7a796ff564
Takahiro KASHIWAGI <takahiro.kashiwagi@enechange.co.jp>
2020-10-05 10:33:59 +0900

    remove variations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :shop
-  #has_many :variations, class_name: 'ItemVariation', foreign_key: 'item_id', dependent: :destroy
-  has_many :item_variations
+  has_many :variations, class_name: 'ItemVariation', foreign_key: 'item_id', dependent: :destroy
   has_many :suppliers, class_name: 'ItemSupplier', foreign_key: 'item_id', dependent: :destroy
 
   enum tax_type: {nomarl: 1, reduced: 2}
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -72,10 +72,11 @@ class Shop < ApplicationRecord
 
     res['variations'].each do |va|
       item.variations.find_or_create_by(base_id: va['variation_id']) do |iva|
-      iva.text = va['variation']
-      iva.stock = va['variation_stock']
-      iva.key = va['variation_identifier']
-      iva.barcode = va['barcode']
+        iva.text = va['variation']
+        iva.stock = va['variation_stock']
+        iva.key = va['variation_identifier']
+        iva.barcode = va['barcode']
+      end
     end
   end
 
